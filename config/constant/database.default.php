b88786a9a7c6a67fc07e65e005eb06cce366940f
GarouDan <dan.garou@gmail.com>
2013-03-29 19:41:39 -0300

    Inserindo arquivo de modelo para o banco de dados.

---
+++ config/constant/database.default.php
@@ -0,0 +1,6 @@
+<?php
+define("DATABASE_USER", "databaseUser");
+define("DATABASE_PASSWORD", "databasePassword");
+define("DATABASE_DATABASE", "databaseName");
+define("DATABASE_HOST", "databaseHost");
+define("DATABASE_SGBD", "databaseSGBD");
