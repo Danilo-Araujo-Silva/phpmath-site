3d66e50e015a36ef07d610c00e464fada89ea594
GarouDan <dan.garou@gmail.com>
2013-03-29 19:36:37 -0300

    Removendo arquivo com as credenciais do banco.

--- config/constant/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("DATABASE_USER", "phpmath");
-define("DATABASE_PASSWORD", "2,718283,1415");
-define("DATABASE_DATABASE", "phpmath");
-define("DATABASE_HOST", "localhost");
-define("DATABASE_SGBD", "mysql");
