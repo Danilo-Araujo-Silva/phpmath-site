76940b133f95bc7f978cde59a102b84df3067b4f
GarouDan <dan.garou@gmail.com>
2013-03-29 22:13:20 -0300

    Creating the Mathematica presentation.

--- index.php
+++ index.php
@@ -3,8 +3,14 @@ require_once 'config/includder.php';
 require_once VENDOR.'autoload.php';
 
 use Backend\Model\Mathematica\Mathematica;
+use Backend\View\Twig\Twig;
+
+$twig = new Twig;
+$data = array();
+$content = $twig->render("phpmath/index.html", $data);
+echo $content;
 
 $mathematica = new Mathematica;
 $mathematica->configure();
 
-echo "<pre>"; print_r($mathematica->run("Fibonacci[200]")); echo "</pre>";
+echo "Fibonacci[200] = <pre>"; print_r($mathematica->run("Fibonacci[200]")); echo "</pre>";
